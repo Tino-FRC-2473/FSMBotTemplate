c85744fb1f624a8cb6f785d68e0b1af4de8b979b
AG6GR <slhe@alumni.princeton.edu>
2023-01-07 16:32:07 -0800

    Add API to check if current board is test setup
    Use DIO pin 9 as indication if the current board is a test setup.
    This allows code to be skipped in testing without the need for
    additional branches.

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 /**
  * HardwareMap provides a centralized spot for constants related to the hardware
  * configuration of the robot.
@@ -15,4 +17,15 @@ public final class HardwareMap {
 	// Pneumatics channel numbers
 	public static final int PCM_CHANNEL_INTAKE_CYLINDER_FORWARD = 1;
 	public static final int PCM_CHANNEL_INTAKE_CYLINDER_REVERSE = 2;
+
+	// Place jumper from DIO pin 9 to GND to indicate this is a test setup
+	private static final int DIO_TEST_SETUP_CHANNEL = 9;
+	private static DigitalInput testBoardPin = new DigitalInput(HardwareMap.DIO_TEST_SETUP_CHANNEL);
+	/**
+	 * Check if the current RoboRIO is part of a test setup or real robot.
+	 * @return true if the current setup is a test setup
+	 */
+	public static boolean isTestBoard() {
+		return !HardwareMap.testBoardPin.get();
+	}
 }
